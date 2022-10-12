58a761689a1e6c91b6c58e63cbdc6af55e2d1c8e
sritipraz <prajapatisriti.sp@gmail.com>
2022-10-12 12:09:53 -0500

    Address, Author, LibraryMebember and Person class

--- src/business/Address.java
+++ src/business/Address.java
@@ -1,8 +1,40 @@
 package business;
 
 public class Address {
-	public String street;
-	public String city;
-	public String state;
-	public String Zip;
+	private String street;
+	private String city;
+	private String state;
+	private Integer zip;
+	public Address(String street, String city, String state, Integer zip) {
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public Integer getZip() {
+		return zip;
+	}
+	public void setZip(Integer zip) {
+		this.zip = zip;
+	}
+	
+
 }
--- src/business/LibraryMember.java
+++ src/business/LibraryMember.java
@@ -1,5 +1,19 @@
 package business;
 
 public class LibraryMember extends Person{
+ private String memberID;
 
+public LibraryMember(String memberID) {
+	super();
+	this.memberID = memberID;
+}
+
+public String getMemberID() {
+	return memberID;
+}
+
+public void setMemberID(String memberID) {
+	this.memberID = memberID;
+}
+ 
 }
