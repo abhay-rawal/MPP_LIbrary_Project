bcc62aebed1da6ee5f10f053477dd3d260a93b77
SD Bappi <sdbappi69@gmail.com>
2022-10-12 11:51:53 -0500

    Book and BookCopy Class

--- src/business/Book.java
+++ src/business/Book.java
@@ -1,5 +1,41 @@
 package business;
 
 public class Book {
-
+	private String isbn;
+	private String author;
+	private String title;
+	private int maxCheckoutLength;
+	
+	public Book(String isbn, String author, String title, int maxCheckoutLength) {
+		super();
+		this.isbn = isbn;
+		this.author = author;
+		this.title = title;
+		this.maxCheckoutLength = maxCheckoutLength;
+	}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getMaxCheckoutLength() {
+		return maxCheckoutLength;
+	}
+	public void setMaxCheckoutLength(int maxCheckoutLength) {
+		this.maxCheckoutLength = maxCheckoutLength;
+	}
 }
--- src/business/BookCopy.java
+++ src/business/BookCopy.java
@@ -1,5 +1,28 @@
 package business;
 
 public class BookCopy {
+	private int copyNo;
+	private Book book;
+	
+	public BookCopy(int copyNo, Book book) {
+		super();
+		this.copyNo = copyNo;
+		this.book = book;
+	}
+	
+	public int getCopyNo() {
+		return copyNo;
+	}
 
+	public void setCopyNo(int copyNo) {
+		this.copyNo = copyNo;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
 }
