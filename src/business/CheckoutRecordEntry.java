31150b4d7baa1fb939185235bb63bd02c2f09604
abhay-rawal <avyrwl1@gmail.com>
2022-10-12 11:57:22 -0500

    Changes to classes

--- src/business/CheckoutRecordEntry.java
+++ src/business/CheckoutRecordEntry.java
@@ -3,7 +3,31 @@ package business;
 import java.time.LocalDate;
 
 public class CheckoutRecordEntry {
-	public CheckoutRecord checkoutRecord;
-	public LocalDate checkoutDate;
-	public LocalDate dueDate;
+	private CheckoutRecord checkoutRecord;
+	private LocalDate checkoutDate;
+	private LocalDate dueDate;
+	public CheckoutRecordEntry(CheckoutRecord checkoutRecord, LocalDate checkoutDate, LocalDate dueDate) {
+		this.checkoutRecord = checkoutRecord;
+		this.checkoutDate = checkoutDate;
+		this.dueDate = dueDate;
+	}
+	public CheckoutRecord getCheckoutRecord() {
+		return checkoutRecord;
+	}
+	public void setCheckoutRecord(CheckoutRecord checkoutRecord) {
+		this.checkoutRecord = checkoutRecord;
+	}
+	public LocalDate getCheckoutDate() {
+		return checkoutDate;
+	}
+	public void setCheckoutDate(LocalDate checkoutDate) {
+		this.checkoutDate = checkoutDate;
+	}
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+	
 }
