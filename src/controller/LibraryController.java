23b316a9762cc507847dd17c1f2f2d58595fb1a5
SD Bappi <sdbappi69@gmail.com>
2022-10-13 15:12:29 -0500

    Optional 1 done

--- src/controller/LibraryController.java
+++ src/controller/LibraryController.java
@@ -37,8 +37,14 @@ public class LibraryController {
 		bookCopyHash = d.readBooksMap();
 	}
 	
-	public void addBook(String isbn, String title, int maxCheckoutLength, List<String> author, int numOfCopies) {
-		
+	public void addBook(String isbn, String title, int maxCheckoutLength, List<Author> author, int numOfCopies) {
+		business.Book newBook = new Book(isbn, title, maxCheckoutLength, author);
+		for(int i = 0; i < numOfCopies; i++) {
+			newBook.addCopy();
+		}
+		HashMap<String,Book> sBook = d.readBooksMap();
+		sBook.put(isbn, newBook);
+		d.updateBooks(sBook);
 	}
 	
 	public void findCheckoutEntry(String memberId) {
--- src/ui/View.java
+++ src/ui/View.java
@@ -1,13 +1,18 @@
 package ui;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import business.CheckoutRecord;
 import controller.LibraryController;
 import controller.LoginController;
 import dataaccess.Auth;
+import business.Address;
+import business.Author;
 
 public final class View {
 	private View() {};
@@ -27,18 +32,64 @@ public final class View {
 		System.out.println("Please enter the maximum checkout length: ");
 		int maxCheckoutLength = op3.nextInt();
 		
+		//Get Author Info
 		Scanner op4 = new Scanner(System.in);  // Create a Scanner object
-		System.out.println("Please enter the authors name with comma(,) separator: ");
-		String author = op4.nextLine();
+		System.out.println("How many authors for this book?:");
+		int authorCount = op4.nextInt();
+		System.out.println("	Please enter the author detail -");
+		List<Author> allAuthors = new ArrayList<Author>();
+		for(int i = 1; i <= authorCount; i++) {
+			System.out.println("	- AUTHOR "+ i +" -");
+			Scanner op41 = new Scanner(System.in);
+			System.out.println("	- Type First name:");
+			String fName = op41.nextLine();
+			
+			Scanner op42 = new Scanner(System.in);
+			System.out.println("	- Type Last name:");
+			String Lname = op42.nextLine();
+			
+			Scanner op43 = new Scanner(System.in);
+			System.out.println("	- Type Phone number:");
+			String pNo = op43.nextLine();
+			
+			// Get Address
+				System.out.println("	Please enter the address detail -");
+				Scanner op441 = new Scanner(System.in);
+				System.out.println("		- Street:");
+				String street = op441.nextLine();
+				
+				Scanner op442 = new Scanner(System.in);
+				System.out.println("		- City:");
+				String city = op442.nextLine();
+				
+				Scanner op443 = new Scanner(System.in);
+				System.out.println("		- State:");
+				String state = op443.nextLine();
+				
+				Scanner op444 = new Scanner(System.in);
+				System.out.println("		- Zip:");
+				String zip = op444.nextLine();
+				
+				Address address = new Address(street, city, state, zip);
+			
+			Scanner op45 = new Scanner(System.in);
+			System.out.println("	- Type Bio:");
+			String shortBio = op45.nextLine();
+			
+			Author author = new Author(fName, Lname, pNo, address, shortBio);
+			allAuthors.add(author);
+			
+			System.out.println("-------------------------------------------");
+		}
 		
 		Scanner op5 = new Scanner(System.in);  // Create a Scanner object
 		System.out.println("Please enter the number of copies: ");
 		int numOfCopies = op5.nextInt();
 		
-		List<String> authors = Arrays.asList(author.split("\\s*,\\s*"));
+//		List<String> authors = Arrays.asList(author.split("\\s*,\\s*"));
 		
 		LibraryController lc = new LibraryController();
-		lc.addBook(isbn, title, maxCheckoutLength, authors, numOfCopies);
+		lc.addBook(isbn, title, maxCheckoutLength, allAuthors, numOfCopies);
 		
 		// To continue
 		Scanner op = new Scanner(System.in);  // Create a Scanner object
